8f2d6de220d733c1d9ad3e1b7bad291488525d40
Brieuc Tribouillet <brieuc.tribouillet@gmail.com>
2024-07-16 21:27:22 +0800

    SessionsSidebar with list of sessions

---
+++ packages/api/modules/chat/procedures/sessions.ts
@@ -0,0 +1,19 @@
+import { ChatSessionSchema, db } from "database";
+import { z } from "zod";
+import { protectedProcedure } from "../../trpc";
+
+export const sessions = protectedProcedure
+  .output(
+    z.array(
+      ChatSessionSchema,
+    ),
+  )
+  .query(async ({ ctx: { user } }) => {
+    const sessions = await db.chatSession.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    return sessions ?? []
+  });
