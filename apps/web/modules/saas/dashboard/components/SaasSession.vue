0bb91c5744ea7f8ccc59e597e0ac91bf9389e2c0
Brieuc Tribouillet <brieuc.tribouillet@gmail.com>
2024-07-18 21:55:21 +0800

    Implementation of openAI assistant WIP

--- apps/web/modules/saas/dashboard/components/SaasSession.vue
+++ apps/web/modules/saas/dashboard/components/SaasSession.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
   import {
     UserIcon,
+    BotIcon
   } from "lucide-vue-next";
-
-
+  
   const props = defineProps({
-    selectedSessionId: {
-      type: String,
-      default: null,
+    selectedSession: {
+      type: Object,
+      required: true,
     },
   });
-
+  
   const { apiCaller } = useApiCaller();
-  const messages = ref<any[]>([]); // Update the type of messages to an array of strings
+  const messages = ref<any[]>([]);
 
   const fetchMessages = async (sessionId: string | null) => {
     if (!sessionId) {
@@ -27,19 +27,19 @@
     }
   };
 
-  const onMessageCreated = (message: string) => {
+  const onMessageCreated = async (message: any) => {
     messages.value.push(message);
-  };
+  }
 
   onMounted(() => {
-    fetchMessages(props.selectedSessionId);
+    fetchMessages(props.selectedSession.id);
   });
 
   watch(
-    () => props.selectedSessionId,
-    (newSessionId) => {
+    () => props.selectedSession,
+    (newSession) => {
       messages.value = [];
-      fetchMessages(newSessionId);
+      fetchMessages(props.selectedSession.id);
     }
   );
 </script>
@@ -48,23 +48,24 @@
 <template>
   <div class="bg-card text-foreground container mt-4 h-full max-w-6xl rounded-lg border p-8">
     <div class="relative flex h-full flex-col">
-      <div class="h-full overflow-y-scroll">
-        <div v-if="selectedSessionId">
+      <div class="h-full overflow-y-scroll pb-24">
+        <div v-if="selectedSession">
           <ul>
-            <li v-for="message in messages" :key="message.id" class="mb-2 flex items-center gap-2">
+            <li v-for="message in messages" :key="message.id" class="mb-2 flex items-start gap-2">
               <div class="rounded-full bg-slate-300 p-2">
                 <UserIcon v-if="message.sender === 'user'" class="text-primary-foreground size-4" />
+                <BotIcon v-else class="text-primary-foreground size-4" />
               </div>
               <div class="flex">
-                <div class="bg-primary whitespace-pre-line rounded-lg px-4 py-2">
-                  <span class="text-primary-foreground break-words">{{ message.text }}</span>
+                <div class="whitespace-pre-line rounded-lg rounded-tl-none px-4 py-2" :class="[message.sender === 'user' ? 'bg-primary' : 'bg-primary/5']">
+                  <span class="max-w-lg  break-words" :class="[message.sender === 'user' ? 'text-primary-foreground' : 'text-primary-background']">{{ message.text }}</span>
                 </div>
               </div>
             </li>
           </ul>
         </div>
       </div>
-      <SaasSessionInput :selectedSessionId="selectedSessionId" @messageCreated="onMessageCreated" />
+      <SaasSessionInput :selectedSession="selectedSession" @messageCreated="onMessageCreated" />
     </div>
   </div>
 </template>
