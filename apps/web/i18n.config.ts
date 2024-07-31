9b26c39c3c5d03860992f73c7fca0cf8c5669336
Brieuc Tribouillet <brieuc.tribouillet@gmail.com>
2024-07-31 16:25:59 +0800

    numberFormats updated

--- apps/web/i18n.config.ts
+++ apps/web/i18n.config.ts
@@ -11,37 +11,7 @@ export default defineI18nConfig(() => ({
   locale: "en",
   fallbackLocale: "en",
   numberFormats: {
-    de: {
-      currency: {
-        style: "currency",
-        currency: "USD",
-        notation: "standard",
-      },
-      number: {
-        style: "decimal",
-        maximumFractionDigits: 0,
-      },
-      percent: {
-        style: "percent",
-        useGrouping: false,
-      },
-    },
     en: {
-      currency: {
-        style: "currency",
-        currency: "USD",
-        notation: "standard",
-      },
-      number: {
-        style: "decimal",
-        maximumFractionDigits: 0,
-      },
-      percent: {
-        style: "percent",
-        useGrouping: false,
-      },
-    },
-    es: {
       currency: {
         style: "currency",
         currency: "USD",
