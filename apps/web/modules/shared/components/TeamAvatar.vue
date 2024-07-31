2428b829101e62221020946a6439409ec108fb21
Brieuc Tribouillet <brieuc.tribouillet@gmail.com>
2024-07-31 15:50:45 +0800

    remove boring avatar

--- apps/web/modules/shared/components/TeamAvatar.vue
+++ apps/web/modules/shared/components/TeamAvatar.vue
@@ -8,8 +8,6 @@
 <template>
   <Avatar>
     <AvatarImage v-if="props.avatarUrl" :src="props.avatarUrl" />
-    <AvatarFallback class="bg-primary/10 text-primary">
-      <BoringAvatar :size="96" :name="name" variant="marble" />
-    </AvatarFallback>
+    <AvatarFallback class="bg-primary/10 text-primary"> </AvatarFallback>
   </Avatar>
 </template>
