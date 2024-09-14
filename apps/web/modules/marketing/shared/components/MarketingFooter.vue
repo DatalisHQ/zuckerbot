0985d0b144cd67f5e017c4d38923a5b642ab2c32
Brieuc Tribouillet <brieuc.tribouillet@gmail.com>
2024-09-14 09:32:38 +0800

    aitools.inc logo added

--- apps/web/modules/marketing/shared/components/MarketingFooter.vue
+++ apps/web/modules/marketing/shared/components/MarketingFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-muted py-12 text-muted-foreground">
+  <footer class="bg-muted text-muted-foreground py-12">
     <div class="container grid grid-cols-1 gap-6 lg:grid-cols-3">
       <div>
         <Logo class="opacity-50 grayscale" />
@@ -20,6 +20,19 @@
         <NuxtLink href="/legal/terms" class="block">
           Terms and conditions
         </NuxtLink>
+      </div>
+      <div>
+        <a
+          href="https://aitools.inc/tools/zuckerbot-ai?utm_source=embed-badge-zuckerbot-ai&utm_medium=embed&utm_campaign=embed-badge-featured"
+          target="_blank"
+          style="width: 175px; height: 54px"
+          ><img
+            src="https://aitools.inc/tools/zuckerbot-ai/embeds/v1/featured-badge.svg?theme=light"
+            alt="ZuckerBot | AI Tools"
+            style="width: 175px; height: 54px"
+            width="175"
+            height="54"
+        /></a>
       </div>
     </div>
   </footer>
