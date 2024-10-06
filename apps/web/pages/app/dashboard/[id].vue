f91826a442a5814f3da2311dff7e0cea877623fb
Brieuc Tribouillet <brieuc.tribouillet@gmail.com>
2024-10-06 20:08:41 +0800

    session page with route param

--- apps/web/pages/app/dashboard/[id].vue
+++ apps/web/pages/app/dashboard/[id].vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
   import type { ChatSession } from "database";
 
-  const { t } = useTranslations();
-  const { user } = useUser();
-
   definePageMeta({
     layout: "saas-chat",
   });
 
+  const route = useRoute();
+  const router = useRouter();
+
+  const { t } = useTranslations();
+  const { user } = useUser();
   const { apiCaller } = useApiCaller();
 
-  const sessions = ref<ChatSession[]>([]);
+  const selectedSession = ref<ChatSession | null>(null);
+  const loading = ref(true);
+  const error = ref<string | null>(null);
   const creatingSession = ref(false);
 
   const paidEmails = ["brieuc@datalis.app", "davis@datalis.app"];
 
   const isPaidUser = computed(() => {
-    // Allow all users to access the dashboard for now
     return true;
 
     // if (user.value) {
@@ -25,27 +28,48 @@
     // return false;
   });
 
+  const fetchSession = async () => {
+    if (!isPaidUser.value) {
+      router.replace("/app/dashboard"); // Redirect to dashboard if not paid
+      return;
+    }
+    try {
+      loading.value = true;
+      const response = await apiCaller.chat.session.query({
+        id: route.params.id,
+      });
+      selectedSession.value = response;
+    } catch (err) {
+      console.error("Error fetching session:", err);
+      error.value = "Failed to load session.";
+    } finally {
+      loading.value = false;
+    }
+  };
+
   const handleSessionCreating = () => {
     creatingSession.value = true;
   };
+
+  onMounted(fetchSession);
 </script>
 
 <template>
   <div class="h-full p-8">
     <div class="flex h-full flex-col items-start gap-8 md:flex-row">
       <template v-if="isPaidUser">
-        <div class="size-full md:max-w-[200px]">
-          <SaasSessionsSidebar
-            :creatingSession="creatingSession"
-            @session-creating="handleSessionCreating"
-          />
-        </div>
-
-        <SaasCreateSession
-          :creatingSession="creatingSession"
-          @session-creating="handleSessionCreating"
-        />
+        <template v-if="selectedSession">
+          <div class="size-full md:max-w-[200px]">
+            <SaasSessionsSidebar
+              @session-creating="handleSessionCreating"
+              :creating-session="creatingSession"
+              :selected-session="selectedSession"
+            />
+          </div>
+          <SaasSession :selectedSession="selectedSession" />
+        </template>
       </template>
+
       <div
         v-else
         class="flex size-full flex-col content-center items-center justify-center"
