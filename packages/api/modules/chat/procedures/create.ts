0bf7b2e1f5ac892da8f010217b25eda60523d4b0
Brieuc Tribouillet <brieuc.tribouillet@gmail.com>
2024-12-04 20:13:06 +0800

    Streaming WIP

--- packages/api/modules/chat/procedures/create.ts
+++ packages/api/modules/chat/procedures/create.ts
@@ -1,9 +1,6 @@
-import OpenAI from "openai";
 import { db } from "database";
 import { z } from "zod";
 import { protectedProcedure } from "../../trpc";
-import { getSignedUrl } from "storage";
-import { assistantConfig } from "utils";
 
 export const create = protectedProcedure
   .input(
@@ -12,45 +9,14 @@ export const create = protectedProcedure
     }),
   )
   .mutation(async ({ input: { name }, ctx: { user } }) => {
-    const openai = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY as string,
-    });
-
-    const knowledgeSignedUrl = await getSignedUrl("/knowledge.json", {
-      bucket: "datalis-avatars",
-      expiresIn: 360,
-    });
-
-    const response = await fetch(knowledgeSignedUrl);
-
-    const knowledgeFile = await openai.files.create({
-      file: response,
-      purpose: "assistants",
-    });
-
-    const vectorStore = await openai.beta.vectorStores.create({
-      name: "ZuckerBot",
-      file_ids: [knowledgeFile.id],
-    });
-
-    const assistant = await openai.beta.assistants.create(assistantConfig);
-    await openai.beta.assistants.update(assistant.id, {
-      tool_resources: { file_search: { vector_store_ids: [vectorStore.id] } },
-    });
-    const thread = await openai.beta.threads.create();
-
     const session = await db.chatSession.create({
       data: {
         name,
         user: { connect: { id: user.id } },
-        assistantId: assistant.id,
-        threadId: thread.id,
       },
       select: {
         id: true,
         name: true,
-        assistantId: true,
-        threadId: true,
         createdAt: true,
       },
     });
